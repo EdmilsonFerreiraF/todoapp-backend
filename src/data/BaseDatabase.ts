cb38ab52d71e0a08a2db052c45c2bddc3754451e
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2021-07-27 20:35:21 -0300

    🎉 :tada: initial commit 🎉
    Folder structure, base database for instanciate database connections
    on queries

---
+++ src/data/BaseDatabase.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+export default class BaseDatabase {
+  protected static uri: any =
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_NAME}`;
+
+  protected static connection: any = 
+      mongoose.connect(BaseDatabase.uri,
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+  protected static closeConnection = function() {
+    // any other clean ups
+    mongoose.connection.close(function () {
+      console.log('Mongoose connection disconnected');
+    });
+  }
+}; 
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.DB_PORT || 3003, () => {
+    console.log("Servidor rodando na porta 3003");
+});
